866bf3c4af6e93c42e84f698428b716dde318d61
WayneNyariroh <wwaynenyariroh@proton.me>
2024-07-20 12:20:04 +0300

    second commit

--- vlapp.py
+++ vlapp.py
@@ -2,53 +2,93 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import datetime
+from datetime import date
+from dateutil.relativedelta import relativedelta
 import time
 
 from streamlit_extras.metric_cards import style_metric_cards
 
-st.set_page_config(
-    page_title="KCCB-ACTs Facility Viral Load Analysis",
-    page_icon="assets\prescription.png",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
+#function to style the page - remove menu, footer and top spacing; set web title, layout
+def page_styling():
+    st.set_page_config(
+        page_title="Facility Viral Load Analysis",
+        page_icon="assets\prescription.png",
+        layout="wide",
+        initial_sidebar_state="expanded")
+    st.markdown(
+        """<style>
+        #MainMenu {visibility:hidden;}
+        footer {visibility:hidden;}
+        </style>""", unsafe_allow_html=True)
+    st.markdown(
+        """<style>
+        .block-container {
+            padding-top: 0.5rem;}</style>""", unsafe_allow_html=True)
+    
+#call the function
+page_styling()
 
-st.markdown(
-    """<style>
-    #MainMenu {visibility:hidden;}
-    footer {visibility:hidden;}
-    </style>""", unsafe_allow_html=True)
-
-st.markdown(
-    """<style>
-    .block-container {
-        padding-top: 0.5rem;}</style>""", unsafe_allow_html=True)
-
+#sidebar
 with st.sidebar:
     cal_watch = datetime.datetime.now()
-    st.button(f':calendar: {cal_watch.strftime("%d/%m/%Y")}', disabled=True)
-    st.subheader('KCCB-ACTs Facility Viral Load Analysis `version 2.0` ')
-    st.caption("mapping of sites supported by ***KCCB-ACTS*** with additional visualizations. "
-                "First tab - :orange[Site Locations] - maps all the facilities, grouping them into proximity clusters depending on zoom level. ")
+    st.button(f' :calendar: {cal_watch.strftime("%d/%m/%Y")}', disabled=True)
+    
+    st.subheader('Facility Viral Load Analysis `version 2.0` ')
+    st.caption("Analyse your facility's client :green[viral loads] within a few seconds with a simple upload")
  
-    upload_csv = st.file_uploader("Upload Recent Active on ART Linelist (CSV only)")
-    if upload_csv is not None:
-        df = pd.read_csv(upload_csv)
-        
-    st.divider()
+    upload_csv = st.file_uploader("Upload Recent Active on ART Linelist (CSV only)", type=['csv'])
+    
     st.markdown('''Made by [Wayne Omondi](https://www.linkedin.com/in/waynewillislink/)
                     ''')
-with st.container:
+if upload_csv is not None:
+    @st.cache
+    def load_csv():
+        csv = pd.read_csv(upload_csv, 
+                            usecols=['CCC No', 'Sex', 'Age at reporting','Art Start Date','Last VL Result','Last VL Date', 'Active in PMTCT', 'Self Visit Date','Next Appointment Date'],
+                            parse_dates=['Art Start Date','Last VL Date','Self Visit Date','Next Appointment Date'])
+        return csv
+    
+    data = load_csv()
+else:
+    #what displays while upload hasnt been done yet
+    st.info("Awaiting linhnelist upload for analysis") 
+
+#st.write(data)
+
+#st.write(data.dtypes)
+    
+    #st.divider()
+data.columns = [x.lower().replace(" ","_") for x in data.columns]
+
+def prep_df(data):
+    return (data
+            .assign(art_start_date = pd.to_datetime(data.art_start_date),
+                    last_vl_date = pd.to_datetime(data.last_vl_date),
+                    self_visit_date = pd.to_datetime(data.self_visit_date),
+                    next_appointment_date = pd.to_datetime(data.next_appointment_date))
+                    )
+df = prep_df(data)
+
+st.write(df)
+
+'''
+elligible_df = df[(df.art_start_date < pd.to_datetime(date.today() + relativedelta(months=-3)))]
+
+#place all components within a container
+with st.container():
     metric1,metric2,metric3 = st.columns(3)
     
     with metric1:
         st.metric(label="TX_Curr",
-                  value=(df.shape[0].astype(str))
+                  value=(df.shape[0])
                   )
     with metric2:
         st.metric(label="Active in PMTCT",
-                value=(df['Active in PMTCT'].eq('Yes').astype(str)))
+                value=(df[df['active_in_pmtct'].eq('Yes')].shape[0]))
 
+    with metric3:
+            st.metric(label="Clients Elligible for VL Uptake",
+                    value=(elligible_df.shape[0]))
 
     st.write(df)
-    
+    '''
